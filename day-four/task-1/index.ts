b67e9dd40b26b4c376bb03f62069cc08154ace10
Kitsunaana <selimkhanov_deni@list.ru>
2025-01-16 15:19:55 +0700

    feat(day-four): added clean user data with ten percent reject

---
+++ day-four/task-1/index.ts
@@ -0,0 +1,67 @@
+const isWithinPercent = (percent: number) => (
+  Math.floor(Math.random() * 100) < Math.min(Math.max(percent, 0), 100)
+)
+
+const sleep = (delay: number) => (
+  new Promise(resolve => (
+    setTimeout(resolve, delay)
+  ))
+)
+
+const sleepWithReject = (delay: number) => (
+  sleep(delay)
+    .then(() => {
+      if (isWithinPercent(10)) {
+        throw new Error("Произошла какая-то ошибка")
+      }
+    })
+)
+
+interface IUser {
+  name: string
+  email: string
+  isActive: boolean
+}
+
+const nameToLowerCase = (name: string) => (
+  name
+    .trim()
+    .toLowerCase()
+)
+
+const emailToLowerCase = (email: string) => email.toLowerCase()
+
+const cleanUserData = (users: IUser[]) => (
+  sleepWithReject(10)
+    .then(() => {
+      return users
+        .filter((user) => user.isActive)
+        .map((user) => {
+          const name = nameToLowerCase(user.name)
+          const email = emailToLowerCase(user.email)
+
+          return {
+            ...user,
+            name,
+            email,
+          }
+        })
+    })
+)
+
+const users: IUser[] = [
+  {
+    email: "kIt@GmAiL.com",
+    name: "Kitsunaana",
+    isActive: true,
+  },
+  {
+    email: "REGINA@mail.ru",
+    name: "Регина",
+    isActive: false,
+  }
+]
+
+cleanUserData(users)
+  .then(console.log)
+  .catch(console.log)
